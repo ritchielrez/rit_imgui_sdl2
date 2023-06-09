629fa48abc38a587989827cf683edf5b89af9a8e
RitchielRez <ritchielrez@gmail.com>
2023-06-09 23:54:17 +0600

    initial commit

---
+++ include/imgui_sdl2.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include "imgui_sdl2.hpp"
+
+#include <iostream>
+
+int main(int argc, char **argv) 
+{
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "SDL Video initilization failed, %s\n", SDL_GetError());
+        return 1;
+    }
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    auto& io = ImGui::GetIO();
+
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+#ifdef WIN32
+    io.Fonts->AddFontFromFileTTF("..\\..\\CascadiaCode.ttf", 18.0f);
+#else
+    io.Fonts->AddFontFromFileTTF("../../CascadiaCode.ttf", 18.0f);
+#endif
+
+    SDL_Window *window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+    ImGui::StyleColorsDark();
+
+    bool running = true;
+    bool somePanel = true;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+                break;
+        }
+
+        ImGui_ImplSDL2_NewFrame();
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui::NewFrame();
+        
+        if (somePanel)
+        {
+            ImGui::Begin("Some panel", &somePanel);
+            ImGui::Text("Test");
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer);
+    }
+
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
+	return 0;
+}
