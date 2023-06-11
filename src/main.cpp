ac2ffa03fbbfe7396baa3db20fa6bcf31aba368b
RitchielRez <ritchielrez@gmail.com>
2023-06-11 14:38:44 +0600

    chore: just some comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,19 @@ int main(int argc, char **argv)
         fprintf(stderr, "SDL Video initilization failed, %s\n", SDL_GetError());
         return 1;
     }
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     auto &io = ImGui::GetIO();
 
+    // Enable keyboard navigation, docking and multiple viewports in imgui
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     io.ConfigDockingWithShift = true;
 
-    io.Fonts->AddFontFromFileTTF("..\\CascadiaCode.ttf", 18.0f);
+    io.Fonts->AddFontFromFileTTF("CascadiaCode.ttf", 18.0f);
 
     SDL_Window *window =
         SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
@@ -31,6 +33,7 @@ int main(int argc, char **argv)
 
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
+
     ImGui::StyleColorsDark();
 
     bool running = true;
@@ -47,11 +50,13 @@ int main(int argc, char **argv)
             break;
         }
 
+        // Tell imgui to create a new frame
         ImGui_ImplSDL2_NewFrame();
         ImGui_ImplSDLRenderer2_NewFrame();
         ImGui::NewFrame();
         ImGui::DockSpaceOverViewport();
 
+        // Create a panel with specific window configuration
         ImGui::SetNextWindowClass(&window_class);
         ImGui::Begin("Some panel", NULL, ImGuiWindowFlags_NoCollapse);
         ImGui::Text("Test");
@@ -64,6 +69,7 @@ int main(int argc, char **argv)
         SDL_RenderPresent(renderer);
     }
 
+    // Destruction!!!!!!!!!!!
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
