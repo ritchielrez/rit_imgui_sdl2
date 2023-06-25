93a4f6617e6e431dc50082ffd3c64007edb278c7
RitchielRez <ritchielrez@gmail.com>
2023-06-25 23:49:43 +0600

    fix: declare rit_imgui_sdl2_app instead of manually qualifying functions and variables with it.

--- src/rit_imgui_sdl2.cpp
+++ src/rit_imgui_sdl2.cpp
@@ -8,15 +8,16 @@
 
 #include <iostream>
 
-SDL_Window *rit_imgui_sdl2_app::window{nullptr};
-SDL_Renderer *rit_imgui_sdl2_app::renderer{nullptr};
+namespace rit_imgui_sdl2_app
+{
+
 namespace
 {
 SDL_Window *window{nullptr};
 SDL_Renderer *renderer{nullptr};
 }; // namespace
 
-void rit_imgui_sdl2_app::init(const char *title, const int width, const int height)
+void init(const char *title, const int width, const int height)
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
@@ -41,27 +42,26 @@ void rit_imgui_sdl2_app::init(const char *title, const int width, const int heig
     // ImGuiWindowClass window_class;
     // window_class.DockNodeFlagsOverrideSet = 1 << 12;
 
-    rit_imgui_sdl2_app::window =
-        SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
-    rit_imgui_sdl2_app::renderer = SDL_CreateRenderer(rit_imgui_sdl2_app::window, -1, SDL_RENDERER_ACCELERATED);
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if (rit_imgui_sdl2_app::window == nullptr)
+    if (window == nullptr)
     {
         std::cerr << "Windows initilization failed, " << SDL_GetError() << "\n";
         return;
     }
 
-    if (rit_imgui_sdl2_app::renderer == nullptr)
+    if (renderer == nullptr)
     {
         std::cerr << "Renderer initilization failed, " << SDL_GetError() << "\n";
         return;
     }
 
-    ImGui_ImplSDL2_InitForSDLRenderer(rit_imgui_sdl2_app::window, rit_imgui_sdl2_app::renderer);
-    ImGui_ImplSDLRenderer2_Init(rit_imgui_sdl2_app::renderer);
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
 }
 
-void rit_imgui_sdl2_app::update()
+void update()
 {
     // Tell imgui to create a new frame
     ImGui_ImplSDL2_NewFrame();
@@ -70,22 +70,23 @@ void rit_imgui_sdl2_app::update()
     ImGui::DockSpaceOverViewport();
 }
 
-void rit_imgui_sdl2_app::render()
+void render()
 {
     ImGui::Render();
-    SDL_RenderClear(rit_imgui_sdl2_app::renderer);
+    SDL_RenderClear(renderer);
     ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
-    SDL_RenderPresent(rit_imgui_sdl2_app::renderer);
+    SDL_RenderPresent(renderer);
 }
 
-void rit_imgui_sdl2_app::exit()
+void exit()
 {
     // Destruction!!!!!!!!!!!
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
 
-    SDL_DestroyWindow(rit_imgui_sdl2_app::window);
-    SDL_DestroyRenderer(rit_imgui_sdl2_app::renderer);
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
 }
+}; // namespace rit_imgui_sdl2_app
