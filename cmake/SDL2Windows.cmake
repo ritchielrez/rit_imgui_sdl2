836d803dfc864795460c57a55d7255fa8df63b2e
RitchielRez <ritchielrez@gmail.com>
2024-03-15 09:04:49 +0600

    fix(cmake): `SDL2-Windows-MinGW/` -> `SDL2.30`
    The user is going to copy the library folder to vendor foloder
    themselves. No need to name the folder in this way.

--- cmake/SDL2Windows.cmake
+++ cmake/SDL2Windows.cmake
@@ -1,8 +1,4 @@
 if(NOT MSVC)
-	set(SDL2_LIB_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL2-Windows-MinGW/lib")
-	set(SDL2_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL2-Windows-MinGW/include")
-	set(SDL2_LIBRARIES "${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL2-Windows-MinGW/bin")
-	include_directories(${SDL2_INCLUDE_DIR} "${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL2-Windows-MinGW/include/SDL2")
 	link_directories(${SDL2_LIB_DIR})
 endif()
 
